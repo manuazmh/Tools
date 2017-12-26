4afca682caaec7c6905efe84a543da417d1861a5
zengminghua <manuazmh@163.com>
2017-12-27 00:18:06 +0800

    (+)function increase：add [ Base Request ].

---
+++ src/com/zmh/util/vo/request/BaseRequest.java
@@ -0,0 +1,14 @@
+package com.zmh.util.vo.request;
+
+import java.io.Serializable;
+
+/**
+ * Base Request
+ *
+ * @Author ZengMinghua
+ * @Date 2017/12/27 0:12
+ */
+public class BaseRequest implements Serializable {
+
+    
+}
